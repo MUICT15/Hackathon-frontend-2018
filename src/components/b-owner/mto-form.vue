77a262a459ad571f6d7ee227dd000516573f683f
isswat <actiondeveloper1998@gmail.com>
2018-03-24 22:32:42 +0700

    add other components

---
+++ src/components/b-owner/mto-form.vue
@@ -0,0 +1,38 @@
+<template>
+<body>
+<div class="container">
+  <h2 align="center">Made To Order</h2>
+  <form action="/action_page.php">
+    <div class="form-group">
+      <label for="pjname">Project name</label>
+      <input type="text" class="form-control" id="pjname" placeholder="Enter project name" name="pjname">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Duration</label>
+      <input type="text" class="form-control" id="pwd" placeholder="Enter duration(days)" name="pswd">
+    </div>
+    <div class="form-group">
+            <label for="pwd">Requirement</label>
+            <input type="text" class="form-control" id="pwd" placeholder="Enter Requirement" name="rqq">
+          </div>
+        
+    <div class="form-check">
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" name="remember"> Remember me
+      </label>
+    </div>
+    <button type="submit" class="btn btn-success">Submit</button>
+  </form>
+</div>
+
+</body>  
+</template>
+<style>
+
+</style>
+<script>
+export default {
+  
+}
+</script>
+
---
+++ src/components/b-owner/ots-form.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+  <h2 align="center">Off the shelf sofwares </h2>
+  <hr>
+  <form action="/action_page.php">
+    <div class="form-group">
+      <label for="text">Project name</label>
+      <input type="text" class="form-control" id="text" placeholder="Enter project name" name="project">
+    </div>
+    <div class="form-group">
+      <label for="text">Duration</label>
+      <input type="text" class="form-control" id="pwd" placeholder="Enter Duration in day" name="text">
+    </div>
+    <label for="text">Choose your business type</label>
+    <div class="form-group">
+            <div class="dropdown">
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                     Business type
+                    </button>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#">Agriculture</a>
+                      <a class="dropdown-item" href="#">Commerce</a>
+                      <a class="dropdown-item" href="#">Industries</a>
+                      <a class="dropdown-item" href="#">Factory</a>
+                    </div>
+            </div>
+    </div>
+    <label for="text">Requirement System</label>
+    <div class="row">
+            <div class="col-sm-12 col-md-12">
+                    <div class="form-check">
+                            <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                            <label for="text">Web Application</label>
+                    </div>
+                    <div class="form-check">
+                            <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                            <label for="text">Mobile Application</label>
+                        </div>
+                    <div class="form-check">
+                            <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                            <label for="text">Network Architecture</label>
+                        </div>
+                    <div class="form-check">
+                            <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                            <label for="text">Management System</label>
+                    </div>
+                    <div class="form-check">
+                            <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                            <label for="text">Intelligent System</label>
+                    </div>
+                    <div class="form-check">
+                            <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                            <label for="text">Storage System</label>
+                    </div>
+            </div>
+    </div>
+    <hr>
+    <button type="submit" class="btn btn-success">Submit</button>
+  </form>
+</div>
+</template>
+<style>
+
+</style>
+<script>
+export default {
+  
+}
+</script>
+
+
